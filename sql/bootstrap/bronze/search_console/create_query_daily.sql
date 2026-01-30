76cd2fb9e7286967362ee0c936ac698136bce524
Khalid <66093648+khldsqmr@users.noreply.github.com>
2026-01-30 10:10:25 -0500

    Determine Error and Resolve

--- sql/bootstrap/bronze/search_console/create_query_daily.sql
+++ sql/bootstrap/bronze/search_console/create_query_daily.sql
@@ -1,4 +1,7 @@
 /*
+
+PARTITION BY expression must be _PARTITIONDATE, DATE(_PARTITIONTIME), DATE(<timestamp_column>), DATE(<datetime_column>), DATETIME_TRUNC(<datetime_column>, DAY/HOUR/MONTH/YEAR), a DATE column, TIMESTAMP_TRUNC(<timestamp_column>, DAY/HOUR/MONTH/YEAR), DATE_TRUNC(<date_column>, MONTH/YEAR), or RANGE_BUCKET(<int64_column>, GENERATE_ARRAY(<int64_value>, <int64_value>[, <int64_value>]))
+
 ===============================================================================
 BRONZE | GOOGLE SEARCH CONSOLE | QUERY LEVEL | ONE-TIME BOOTSTRAP
 ===============================================================================
