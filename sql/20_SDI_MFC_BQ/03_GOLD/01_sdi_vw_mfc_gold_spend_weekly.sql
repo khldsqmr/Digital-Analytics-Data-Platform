2df2c20bcc18bca6fdf650444f39957f247da76a
Khalid Qamar <your-email@example.com>
2026-05-28 18:05:23 -0400

    Add Logical Layers for MFC Spend to bring in Agency

--- sql/20_SDI_MFC_BQ/03_GOLD/01_sdi_vw_mfc_gold_spend_weekly.sql
+++ sql/20_SDI_MFC_BQ/03_GOLD/01_sdi_vw_mfc_gold_spend_weekly.sql
@@ -7,9 +7,22 @@ SELECT
   QGP_Week,
   Quarter_End_Date,
   FileLoad_Date,
-  weekly_actual   AS spend_actual,
-  weekly_forecast AS spend_forecast,
-  weekly_display  AS spend_display,
-  UPPER(TRIM(week_type)) AS week_type
-FROM `prj-dbi-prd-1.ds_dbi_digitalmedia_automation.sdi_vw_mfc_silver_spend_weekly`
-ORDER BY Quarter DESC, QGP_Week DESC;
+  UPPER(TRIM(LOB_Supported))     AS LOB_Supported,
+  SUM(weekly_actual)             AS spend_actual,
+  SUM(weekly_forecast)           AS spend_forecast,
+  SUM(weekly_display)            AS spend_display,
+  UPPER(TRIM(week_type))         AS week_type
+FROM `prj-dbi-prd-1.ds_dbi_digitalmedia_automation.sdi_vw_mfc_silver_spendGranular_weekly`
+GROUP BY
+  Quarter,
+  Period_Start,
+  Period_End,
+  QGP_Week,
+  Quarter_End_Date,
+  FileLoad_Date,
+  UPPER(TRIM(LOB_Supported)),
+  UPPER(TRIM(week_type))
+ORDER BY
+  Quarter DESC,
+  QGP_Week DESC,
+  LOB_Supported;
